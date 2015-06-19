3ac4df5f6ab16e1f4193e119d3439a367ded663f
Abhishek Patel <dev.abhishek19@gmail.com>
2015-06-19 13:09:48 -0400

    added a new create and delete actions to the project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
@@ -6,4 +7,38 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by_id(params[:id])
   end
+
+
+  def new
+    @post = Post.new
+  end
+
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "You have successfully created a new Post!"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Enable to create Post please try again!"
+      render :new
+    end
+  end
+
+
+  def destroy
+    @post = Post.find_by_id(params[:id])
+    if @post.destroy
+      flash[:success] = "You have successfully deleted Post!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Enable to delete Post please try again!"
+      render posts_path
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :post)
+  end
+
 end
